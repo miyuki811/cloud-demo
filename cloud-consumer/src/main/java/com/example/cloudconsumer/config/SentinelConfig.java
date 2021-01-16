b53e51e4c5bb18dca8a64c9bef2325f89560d0ae
peachl <peachl@outlook.com>
2021-01-16 16:05:18 +0800

    新增Sentinel相关配置

---
+++ cloud-consumer/src/main/java/com/example/cloudconsumer/config/SentinelConfig.java
@@ -0,0 +1,18 @@
+package com.example.cloudconsumer.config;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author zjw
+ * @Date 2021/1/16 14:56
+ * @DESC
+ */
+@Configuration
+public class SentinelConfig {
+
+    public SentinelConfig() {
+
+    }
+
+}
